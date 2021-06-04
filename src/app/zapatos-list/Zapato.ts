fc66e149657ded2d662564af60677fcbbedc6bce
Mariano Montero <monterosmarianos@hotmail.com.ar>
2021-06-04 19:50:03 -0300

    video 3, cartel stock, bordes y fondo por oferta

---
+++ src/app/zapatos-list/Zapato.ts
@@ -0,0 +1,8 @@
+export interface Zapato{
+    name: string;
+    type: string;
+    price: number;
+    stock: number;
+    image: string;
+    clearance: boolean;
+}
--- src/app/zapatos-list/zapatos-list.component.ts
+++ src/app/zapatos-list/zapatos-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Zapato} from './Zapato';
 
 @Component({
   selector: 'app-zapatos-list',
@@ -6,12 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./zapatos-list.component.css']
 })
 export class ZapatosListComponent implements OnInit {
-  zapatos = {
-    "nombre": "Nike",
-    "tipo": "Running",
-    "precio": "$12.000",
-    "stock": "13",
-  }
+  zapatos: Zapato[] = [
+    {
+    name: "Nike",
+    type: "Running",
+    price: 12000,
+    stock: 13,
+    image: "assets/img/nike_running.jpg",
+    clearance: false
+  },
+  {
+    name: "Salomon",
+    type: "Trekking",
+    price: 8500,
+    stock: 32,
+    image: "assets/img/salomon_trekking.jpg",
+    clearance: false
+  },
+  {
+    name: "Adidas",
+    type: "Street",
+    price: 14350,
+    stock: 0,
+    image: "assets/img/adidas_street.jpg",
+    clearance: true
+  },
+  ];
 
   constructor() { }
 
